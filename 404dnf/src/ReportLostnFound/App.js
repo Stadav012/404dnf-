29a3ec6c355ae5c34365b7175c66f51aa3ef902e
Stadav01 <stanley.ndlovu@ashesi.edu.gh>
2024-10-22 14:02:55 +0000

    New structure

--- 404dnf/src/ReportLostnFound/App.js
+++ 404dnf/src/ReportLostnFound/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Statistics from './Statistics/Statistics';
@@ -6,8 +5,6 @@ import Statistics from './Statistics/Statistics';
 function App() {
   return (
     <div className="App">
-
-      <Statistics />
     </div>
   );
 }
