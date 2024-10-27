34aadaab538029c38ce860df6427a931f1170eb2
Stadav01 <stanley.ndlovu@ashesi.edu.gh>
2024-10-27 11:24:09 +0000

    Added bento grid

--- 404dnf/src/App.jsx
+++ 404dnf/src/App.jsx
@@ -1,24 +1,11 @@
 import './App.css';
-import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider";
 import Sidebar from './sidebar/Sidebar';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import { ThemeProvider } from "@/components/theme-provider"
-
-import Section from './Sections/Sections';
-
-import Statistics from './Statistics/Statistics';
-
-// import { LampVideo } from '@/components/ui/lamp';
-
-import { BackgroundBeamsWithCollision } from '@/components/ui/background-beams-with-collision';
-
 import Video from './Video/video';
-
-import { LampContainer } from "@/components/ui/lamp";
-
-
-
-
+import Statistics from './Statistics/Statistics';
+import Section from './Sections/Sections';
+import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
+import { IconClipboardCopy, IconTrophy, IconBox } from "@tabler/icons-react";
 
 function App() {
   const awards = [
@@ -27,7 +14,7 @@ function App() {
   ];
 
   const lockers = [
-    { label: 'View available lockers', description: 'Locker number: 12', icon: 'bars' },
+    { label: 'View available lockers', description: 'Locker number: 12', icon: 'box' },
     { label: 'View available lockers', description: 'Locker number: 12', icon: 'compass' }
   ];
 
@@ -37,23 +24,48 @@ function App() {
         <Sidebar />
         
         <div className='content'>
-        {/* <ImageUpload/> */}       {/* I believe this was meant for testing, I removed to run a few tests*/}
-          <Video />
-            {/* <LampVideo className="lamp-video"/> */}
+          <BentoGrid className="max-w-5xl mx-auto md:auto-rows-[30rem] gap-y-24 p-5">
+            {/* Video Item */}
+            <BentoGridItem
+              title="Item Overview Video"
+              description="Watch an overview of all features."
+              header={<Video />}
+              className="md:col-span-2 h-[35rem]"
+              icon={<IconClipboardCopy className="h-4 w-4 text-neutral-500" />}
+            />
 
-          <div className='stats'>
-            <Statistics title="Total Items Found" stats="100" />
-            <Statistics title="Total Items Lost" stats="30" />
-          </div>
+            {/* Awards Item */}
+            <BentoGridItem
+              title="Awards"
+              description="Achievements and recognitions."
+              header={<Section heading="Awards" pills={awards} />}
+              className="md:col-span-1 h-[35rem]"
+              icon={<IconTrophy className="h-4 w-4 text-neutral-500" />}
+            />
 
-  
-          <div className='sections z-20'>
-            <Section heading="Awards" pills={awards} />
-              <Section heading="Smart Locker" pills={lockers} />
-          </div>
-          
-
-          
+            {/* Smart Locker Access Item */}
+            <BentoGridItem
+              title="Smart Locker Access"
+              description="Access to smart locker details."
+              header={<Section heading="Smart Locker" pills={lockers} />}
+              className="md:col-span-1 h-[35rem]"
+              icon={<IconBox className="h-4 w-4 text-neutral-500" />}
+            />
+            
+          {/* Statistics Overview Item */}
+          <BentoGridItem
+            title="Statistics Overview"
+            description="Statistics of items found and lost."
+            header={
+              <div className='stats'>
+                <Statistics title="Total Items Found" stats="100" />
+                <Statistics title="Total Items Lost" stats="30" />
+              </div>
+            }
+            className="md:col-span-2 h-[35rem]"
+            icon={<IconTrophy className="h-4 w-4 text-neutral-500" />}
+            />
+            </BentoGrid>
         </div>
       </div>
     </ThemeProvider>
