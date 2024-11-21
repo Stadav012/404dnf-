f9b029fabdf39c2d6823246c451e446314f7986e
Shammah Dzwairo <shammah.dzwairo@ashesi.edu.gh>
2024-11-21 23:34:26 +0000

    Added database config

---
+++ 404dnf/src/Backend/db/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "404dnf";
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+if ($conn){
+    echo '404dnf Connected Successfully';
+} else {
+    die('Failed to connect to database');
+}
+
+?>
