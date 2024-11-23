c669a61eb26c6f06c9b47a4798e57e7648e9b239
Fadzai00 <fadzai.z@usapschool.org>
2024-11-23 19:00:06 +0000

    added the SQL file

---
+++ 404dnf/src/Backend/db/404dnf.sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 23, 2024 at 07:53 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lostandfound`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `found_locations`
+--
+
+CREATE TABLE `found_locations` (
+  `found_location_id` int(11) NOT NULL,
+  `found_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items_found`
+--
+
+CREATE TABLE `items_found` (
+  `found_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_description` text NOT NULL,
+  `found_date` datetime DEFAULT current_timestamp(),
+  `photo_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE `locations` (
+  `location_id` int(11) NOT NULL,
+  `location_name` enum('location_1','location_2','location_3') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `report_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_description` text NOT NULL,
+  `report_date` datetime DEFAULT current_timestamp(),
+  `status` enum('Open','Closed') DEFAULT 'Open',
+  `location_id` int(11) DEFAULT NULL,
+  `photo_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_found`
+--
+
+CREATE TABLE `report_found` (
+  `match_id` int(11) NOT NULL,
+  `report_id` int(11) NOT NULL,
+  `found_id` int(11) NOT NULL,
+  `match_date` datetime DEFAULT current_timestamp(),
+  `claim_status` enum('Pending','Verified','Rejected') DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_locations`
+--
+
+CREATE TABLE `report_locations` (
+  `report_location_id` int(11) NOT NULL,
+  `report_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rewards`
+--
+
+CREATE TABLE `rewards` (
+  `reward_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_found_items` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `fname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `last_login` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `profile_pic` varchar(255) DEFAULT NULL,
+  `theme` enum('vid1','vid2') NOT NULL DEFAULT 'vid1',
+  `role` enum('admin','regular') NOT NULL DEFAULT 'regular'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_found_items`
+--
+
+CREATE TABLE `user_found_items` (
+  `user_found_item_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `num_items_found` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `found_locations`
+--
+ALTER TABLE `found_locations`
+  ADD PRIMARY KEY (`found_location_id`),
+  ADD KEY `found_id` (`found_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indexes for table `items_found`
+--
+ALTER TABLE `items_found`
+  ADD PRIMARY KEY (`found_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`location_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indexes for table `report_found`
+--
+ALTER TABLE `report_found`
+  ADD PRIMARY KEY (`match_id`),
+  ADD KEY `report_id` (`report_id`),
+  ADD KEY `found_id` (`found_id`);
+
+--
+-- Indexes for table `report_locations`
+--
+ALTER TABLE `report_locations`
+  ADD PRIMARY KEY (`report_location_id`),
+  ADD KEY `report_id` (`report_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indexes for table `rewards`
+--
+ALTER TABLE `rewards`
+  ADD PRIMARY KEY (`reward_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `user_found_items`
+--
+ALTER TABLE `user_found_items`
+  ADD PRIMARY KEY (`user_found_item_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `found_locations`
+--
+ALTER TABLE `found_locations`
+  MODIFY `found_location_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `items_found`
+--
+ALTER TABLE `items_found`
+  MODIFY `found_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report_found`
+--
+ALTER TABLE `report_found`
+  MODIFY `match_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report_locations`
+--
+ALTER TABLE `report_locations`
+  MODIFY `report_location_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rewards`
+--
+ALTER TABLE `rewards`
+  MODIFY `reward_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_found_items`
+--
+ALTER TABLE `user_found_items`
+  MODIFY `user_found_item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `found_locations`
+--
+ALTER TABLE `found_locations`
+  ADD CONSTRAINT `found_locations_ibfk_1` FOREIGN KEY (`found_id`) REFERENCES `items_found` (`found_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `found_locations_ibfk_2` FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `items_found`
+--
+ALTER TABLE `items_found`
+  ADD CONSTRAINT `items_found_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `reports_ibfk_2` FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `report_found`
+--
+ALTER TABLE `report_found`
+  ADD CONSTRAINT `report_found_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `reports` (`report_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `report_found_ibfk_2` FOREIGN KEY (`found_id`) REFERENCES `items_found` (`found_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `report_locations`
+--
+ALTER TABLE `report_locations`
+  ADD CONSTRAINT `report_locations_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `reports` (`report_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `report_locations_ibfk_2` FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `rewards`
+--
+ALTER TABLE `rewards`
+  ADD CONSTRAINT `rewards_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_found_items`
+--
+ALTER TABLE `user_found_items`
+  ADD CONSTRAINT `user_found_items_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
